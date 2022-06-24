479e93546518be5e49f7a29773bbc62016e11a34
Collin Diekvoss <collin@diekvoss.com>
2022-06-24 10:27:08 -0500

    instance

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #define GLFW_INCLUDE_VULKAN
-
 #include <GLFW/glfw3.h>
 
 #include <cstdlib>
 #include <iostream>
 #include <stdexcept>
+#include <vector>
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
@@ -27,6 +27,51 @@ private:
   }
 
   void initVulkan() {
+    createInstance();
+  }
+
+  void createInstance() {
+    VkApplicationInfo appInfo{};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Hello Vulkan!";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    VkInstanceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    uint32_t glfwExtensionCount = 0;
+    const char **glfwExtensions;
+
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+    createInfo.enabledExtensionCount = glfwExtensionCount;
+    createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+    createInfo.enabledLayerCount = 0;
+
+    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+      throw std::runtime_error("failed to create instance!");
+    }
+
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+    std::vector<VkExtensionProperties> extensions(extensionCount);
+
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+    std::cout << "available extensions:\n";
+    for (const auto &extension: extensions) {
+      std::cout << "    " << extension.extensionName << '\n';
+    }
+
+    uint32_t requiredExtensionCount = 0;
+    const char **requiredExtensions = glfwGetRequiredInstanceExtensions(&requiredExtensionCount);
+    std::cout << "required extensions: " << *requiredExtensions << '\n';
   }
 
   void mainLoop() {
@@ -36,11 +81,14 @@ private:
   }
 
   void cleanup() {
+    vkDestroyInstance(instance, nullptr);
     glfwDestroyWindow(window);
     glfwTerminate();
   }
 
+private:
   GLFWwindow *window;
+  VkInstance instance;
 };
 
 int main() {
