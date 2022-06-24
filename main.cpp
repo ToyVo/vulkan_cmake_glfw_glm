7a11badbdc60be24c086d6967b02492777ce5eba
Collin Diekvoss <collin@diekvoss.com>
2022-06-24 10:27:07 -0500

    create window

--- main.cpp
+++ main.cpp
@@ -1,6 +1,57 @@
+#define GLFW_INCLUDE_VULKAN
+
+#include <GLFW/glfw3.h>
+
+#include <cstdlib>
 #include <iostream>
+#include <stdexcept>
+
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
+class HelloTriangleApplication {
+public:
+  void run() {
+    initWindow();
+    initVulkan();
+    mainLoop();
+    cleanup();
+  }
+
+private:
+  void initWindow() {
+    glfwInit();
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    window = glfwCreateWindow(WIDTH, HEIGHT, "Hello Vulkan!", nullptr, nullptr);
+  }
+
+  void initVulkan() {
+  }
+
+  void mainLoop() {
+    while (!glfwWindowShouldClose(window)) {
+      glfwPollEvents();
+    }
+  }
+
+  void cleanup() {
+    glfwDestroyWindow(window);
+    glfwTerminate();
+  }
+
+  GLFWwindow *window;
+};
 
 int main() {
-  std::cout << "Hello Vulkan!\n";
-  return EXIT_FAILURE;
+  HelloTriangleApplication app{};
+
+  try {
+    app.run();
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
 }
